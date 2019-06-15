96ba30ab090d357fe4509410c974bce133c6eabf
shingyoza <shingyoza@gmail.com>
2019-06-15 11:12:42 -0700

    Solved 66. Plus One

---
+++ solution/js/66-Plus-One.js
@@ -0,0 +1,20 @@
+// Input: [1,2,3]
+// Output: [1,2,4]
+// Explanation: The array represents the integer 123.
+
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    for (let i=digits.length-1; i>=0; i--) {
+        if (digits[i] < 9) {
+            digits[i] += 1;
+            return digits;
+        } else {
+            digits[i] = 0;
+        }
+    }
+    digits.unshift(1);
+    return digits;
+};
