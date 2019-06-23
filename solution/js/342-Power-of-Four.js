5468bfa43a65d16ffba07dc2ab4e64fcc4b7710a
shingyoza <shingyoza@gmail.com>
2019-06-22 19:07:49 -0700

    Solved 342. Power of Four

---
+++ solution/js/342-Power-of-Four.js
@@ -0,0 +1,13 @@
+// Input: 16
+// Output: true
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPowerOfFour = function(num) {
+    while (num >= 4) {
+        num /= 4;
+    }
+    return num === 1;
+};
